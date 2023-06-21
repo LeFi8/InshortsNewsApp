79c309944f2768bc91fe55ebea8ea0a29b8f2586
lef8 <alan.sipiorski14@gmail.com>
2023-06-21 19:44:43 +0200

    add news item data class

---
+++ app/src/main/java/com/example/inshortsnewsapp/model/NewsItem.kt
@@ -0,0 +1,10 @@
+package com.example.inshortsnewsapp.model
+
+data class NewsItem(
+    val date: String,
+    val id: String,
+    val imageUrl: String,
+    val readMoreUrl: String,
+    val title: String,
+)
+
