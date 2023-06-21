838a87a6cb2fa5df7b27a70d7054c378ce1b2295
lef8 <alan.sipiorski14@gmail.com>
2023-06-21 17:59:46 +0200

    add date to activity main screen

--- app/src/main/java/com/example/inshortsnewsapp/MainActivity.kt
+++ app/src/main/java/com/example/inshortsnewsapp/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.inshortsnewsapp
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import com.example.inshortsnewsapp.authentication.FirebaseManager
+import com.example.inshortsnewsapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 
 class MainActivity : Activity() {
 
     private lateinit var firebaseManager: FirebaseManager
+    private lateinit var binding: ActivityMainBinding
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         firebaseManager = FirebaseManager()
-        setContentView(R.layout.activity_main)
 
         val isLoggedIn = firebaseManager.getUser() != null
         if (!isLoggedIn) {
@@ -20,6 +24,19 @@ class MainActivity : Activity() {
             finish()
         }
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val date = SimpleDateFormat("dd/MM/yyyy").format(System.currentTimeMillis())
+        binding.todaysDate.text = getString(R.string.todays_date, date)
+
+    }
+    override fun onStart() {
+        super.onStart()
+        loadNews()
+    }
+
+    private fun loadNews() {
 
     }
 }
