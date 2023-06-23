d3d0965624cfc952e3955387ae3cec254ec3a284
lef8 <alan.sipiorski14@gmail.com>
2023-06-23 12:25:11 +0200

    rm unimportant imports

--- app/src/main/java/com/example/inshortsnewsapp/MainActivity.kt
+++ app/src/main/java/com/example/inshortsnewsapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.inshortsnewsapp.adapter.NewsAdapter
 import com.example.inshortsnewsapp.firebase.FirebaseManager
